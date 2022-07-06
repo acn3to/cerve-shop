eefe7364f459e21419d77e181907129281a3aaed
Arnaldo Neto <costanetoow@gmail.com>
2022-07-06 00:48:36 -0300

    chore: configure absolute imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Menu from './pages/Menu';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <div>Beer Shop</div>
+    <Menu/>
   </React.StrictMode>,
 );
--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,5 +1,5 @@
 import styles from "./Menu.module.scss"
-import {ReactComponent as Logo} from "../../assets/logo.svg"
+import {ReactComponent as Logo} from "assets/logo.svg"
 
 export default function Menu() {
    return (<main>
