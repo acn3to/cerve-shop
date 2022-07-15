b926ac5f53fa8ec05be54f11ffc5922475f257a9
Arnaldo Neto <costanetoow@gmail.com>
2022-07-14 23:16:00 -0300

    feat: define a state for menu

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,7 +1,10 @@
 import styles from "./Menu.module.scss"
 import {ReactComponent as Logo} from "assets/logo.svg"
+import Searcher from './Searcher/';
+import { useState } from "react";
 
 export default function Menu() {
+  const [search, setSearch] = useState("")
    return (<main>
     <nav className={styles.menu}>
       <Logo/>
@@ -9,5 +12,9 @@ export default function Menu() {
     <header className={styles.header}>
       <div className={styles.header__text}>A sua loja virtual de cervejas artesanais importadas e nacionais.</div>
     </header>
+    <section className={styles.menu}>
+      <h3 className={styles.menu__title}>O que você procura?</h3>
+      <Searcher search={search} setSearch={setSearch}/>
+    </section>
    </main>)
 }
