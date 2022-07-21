3f61fc2ee6a0c6fa8c31f93da422c4143136803f
Arnaldo Neto <costanetoow@gmail.com>
2022-07-21 16:25:00 -0300

    refactor: moves menu component to routes

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,10 +1,33 @@
 import { ReactComponent as Logo } from 'assets/logo.svg'
+import { Link } from 'react-router-dom'
+
 import styles from './Menu.module.scss'
 
 export default function Menu() {
+  const routes = [
+    {
+      label: 'Início',
+      to: '/',
+    },
+    {
+      label: 'Produtos',
+      to: '/produtos',
+    },
+    {
+      label: 'Sobre',
+      to: '/sobre',
+    },
+  ]
   return (
     <nav className={styles.menu}>
       <Logo />
+      <ul className={styles.menu__list}>
+        {routes.map((route, index) => (
+          <li className={styles.menu__link} key={index}>
+            <Link to={route.to}>{route.label}</Link>
+          </li>
+        ))}
+      </ul>
     </nav>
   )
 }
--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,4 @@
+import Menu from 'components/Menu'
 import Home from 'pages/Home'
 import Products from 'pages/Products'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
@@ -5,6 +6,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 export default function AppRouter() {
   return (
     <Router>
+      <Menu />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/produtos" element={<Products />} />
