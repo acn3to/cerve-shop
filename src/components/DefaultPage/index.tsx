9062448358603c59d5791a9d4476827328ed16e6
Arnaldo Neto <costanetoow@gmail.com>
2022-07-22 16:14:49 -0300

    feat: creates theme style page

--- src/components/DefaultPage/index.tsx
+++ src/components/DefaultPage/index.tsx
@@ -2,15 +2,17 @@ import { Outlet } from 'react-router-dom'
 
 import styles from './DefaultPage.module.scss'
 
+import stylesTheme from 'styles/Theme.module.scss'
+
 export default function DefaultPage() {
   return (
     <>
       <header className={styles.header}>
         <div className={styles.header__text}>
-          A sua loja virtual de cervejas artesanais importadas e nacionais.
+          A sua loja virtual de cervejas artesanais importadas e nacionais
         </div>
       </header>
-      <div>
+      <div className={stylesTheme.container}>
         <Outlet />
       </div>
     </>
