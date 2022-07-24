55def57567814d9c1c147c844ea4f40cae5c478c
Arnaldo Neto <costanetoow@gmail.com>
2022-07-24 13:21:53 -0300

    feat: add assets and address to home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,9 @@
-import Menu from 'components/Menu'
 import menu from 'data/menu.json'
+import stylesTheme from 'styles/Theme.module.scss'
+
 import styles from './Home.module.scss'
+
+import ourHome from 'assets/our_home.png'
 
 export default function Home() {
   let recommendedItems = [...menu]
@@ -10,8 +13,7 @@ export default function Home() {
 
   return (
     <section>
-      <Menu />
-      <h3 className={styles.title}>Recomendações do mestre cervejeiro</h3>
+      <h3 className={stylesTheme.title}>Recomendações do mestre cervejeiro</h3>
       <div className={styles.recommendeds}>
         {recommendedItems.map((item) => (
           <div key={item.id} className={styles.recommended}>
@@ -21,6 +23,13 @@ export default function Home() {
             <button className={styles.recommended__button}>Ver mais</button>
           </div>
         ))}
+      </div>
+      <h3 className={stylesTheme.title}> Nossa casa </h3>
+      <div className={styles.ourHome}>
+        <img src={ourHome} alt="Imagem de um bar" />
+        <div className={styles.ourHome__address}>
+          Alameda Tabajara, 6 <br /> <br /> Salvador - BA
+        </div>
       </div>
     </section>
   )
