337f37418d3afefee8cd764eb8cbc86791faf0e9
Arnaldo Neto <costanetoow@gmail.com>
2022-08-19 18:14:53 -0300

    feat: add media queries for notfound page

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,19 +1,12 @@
 import { ReactComponent as NotFoundImage } from 'assets/not_found.svg'
-import classNames from 'classnames'
-import stylesTheme from 'styles/Theme.module.scss'
+import { useNavigate } from 'react-router-dom'
 
 import styles from './NotFound.module.scss'
-import { useNavigate } from 'react-router-dom'
 
 export default function NotFound() {
   const navigate = useNavigate()
   return (
-    <div
-      className={classNames({
-        [styles.container]: true,
-        [stylesTheme.container]: true,
-      })}
-    >
+    <div className={styles.container}>
       <div className={styles.back}>
         <button onClick={() => navigate(-1)}>{'< Voltar'}</button>
       </div>
