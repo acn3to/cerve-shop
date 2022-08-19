0e4b9dfdcb23d965d46794aeb410c808e07c5e21
Arnaldo Neto <costanetoow@gmail.com>
2022-08-19 18:18:27 -0300

    feat: add media queries for item component

--- src/pages/Products/Items/Item/index.tsx
+++ src/pages/Products/Items/Item/index.tsx
@@ -1,12 +1,14 @@
 import Tags from 'components/Tags'
+import { useNavigate } from 'react-router-dom'
 import { Product } from 'types/Product'
 
 import styles from './Item.module.scss'
 
 export default function Item(props: Product) {
-  const { title, description, photo } = props
+  const { id, title, description, photo } = props
+  const navigate = useNavigate()
   return (
-    <div className={styles.item}>
+    <div className={styles.item} onClick={() => navigate(`/produto/${id}`)}>
       <div className={styles.items__image}>
         <img src={photo} alt={title} />
       </div>
