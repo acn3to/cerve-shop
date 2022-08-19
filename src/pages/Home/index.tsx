19e7c946b383b47269866063f3a98751058f93cd
Arnaldo Neto <costanetoow@gmail.com>
2022-08-19 18:14:30 -0300

    feat: add media queries for home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,6 @@
 import ourHome from 'assets/our_home.png'
 import menu from 'data/menu.json'
 import { useNavigate } from 'react-router-dom'
-import stylesTheme from 'styles/Theme.module.scss'
 import { Product } from 'types/Product'
 
 import styles from './Home.module.scss'
@@ -20,27 +19,29 @@ export default function Home() {
 
   return (
     <section>
-      <h3 className={stylesTheme.title}>Recomendações do mestre cervejeiro</h3>
-      <div className={styles.recommendeds}>
-        {recommendedItems.map((item) => (
-          <div key={item.id} className={styles.recommended}>
-            <div className={styles.recommended__image}>
-              <img src={item.photo} alt={item.title} />
+      <div className={styles.container}>
+        <h3 className={styles.title}>Recomendações do mestre cervejeiro</h3>
+        <div className={styles.recommendeds}>
+          {recommendedItems.map((item) => (
+            <div key={item.id} className={styles.recommended}>
+              <div className={styles.recommended__image}>
+                <img src={item.photo} alt={item.title} />
+              </div>
+              <button
+                className={styles.recommended__button}
+                onClick={() => redirectToDetails(item)}
+              >
+                Ver mais
+              </button>
             </div>
-            <button
-              className={styles.recommended__button}
-              onClick={() => redirectToDetails(item)}
-            >
-              Ver mais
-            </button>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-      <h3 className={stylesTheme.title}> Nossa casa </h3>
+      <h3 className={styles.ourHome_title}> Nossa casa </h3>
       <div className={styles.ourHome}>
         <img src={ourHome} alt="Imagem de um bar" />
         <div className={styles.ourHome__address}>
-          Alameda Tabajara, 6 <br /> <br /> Salvador - BA
+          Paralelo, 14 <br /> <br /> Alto Paraíso de Goiás - GO
         </div>
       </div>
     </section>
